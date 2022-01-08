d3097dcfa363925db06a6759640df210e01892ba
AHTHneeuhl <ahthneeuhl@gmail.com>
2022-01-08 14:02:09 +0530

    firebase updated:

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,12 +2,12 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD8ozWoZ2vWiZUdiNpN4IODHj2eYduYlu0",
-  authDomain: "reactposts-2c6e0.firebaseapp.com",
-  projectId: "reactposts-2c6e0",
-  storageBucket: "reactposts-2c6e0.appspot.com",
-  messagingSenderId: "818201475700",
-  appId: "1:818201475700:web:0d2e95872d782967e1af21",
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
 };
 
 firebase.initializeApp(firebaseConfig);
